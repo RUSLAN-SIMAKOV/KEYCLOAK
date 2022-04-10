f6630140f6ee6cdf47e38bc0b401dc08da458213
Ruslan Simakov <ruslan.simakov@spirent.com>
2022-04-10 15:48:27 +0100

    Create Scope Based SS

---
+++ resource-server/src/main/java/ruslan/simakov/resourceserver/security/WebSecurity.java
@@ -0,0 +1,21 @@
+package ruslan.simakov.resourceserver.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/user/*")
+                .hasAuthority("SCOPE_profile")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
+}
